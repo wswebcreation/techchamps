d3d4144d3805bdcfb99d6e6c21a6088bc0b65d73
Wim Selles <wim@wswebcreation.nl>
2023-04-02 14:31:29 +0200

    add login test

--- tests/specs/mobile.spec.ts
+++ tests/specs/mobile.spec.ts
@@ -30,12 +30,39 @@ describe('My First Native App Test', () => {
   it.only('Should be able to login', async () => {
     //
     // 1. Open the menu
+    const menuSelector = driver.isIOS ? '~tab bar option menu' : '~open menu';
+    await $(menuSelector).click();
     //
     // 2. Go to Login
+    const loginMenuSelector = '~menu item log in';
+    await $(loginMenuSelector).waitForDisplayed();
+    await $(loginMenuSelector).click();
     //
     // 3. Login
+    const usernameSelector = driver.isIOS
+      ? '~Username'
+      : '//android.widget.EditText[1]';
+    const passwordSelector = driver.isIOS
+      ? '~Password'
+      : '//android.widget.EditText[2]';
+    await $(usernameSelector).waitForDisplayed();
+    await $(usernameSelector).setValue('bob@example.com');
+    //
+    // !! TIP !!
+    // Think about smaller screens and how to make sure the keyboard is not covering the input field
+    await hideKeyboard();
+    await $(passwordSelector).setValue('10203040');
+    //
+    // !! TIP !!
+    // Think about smaller screens and how to make sure the keyboard is not covering the submit button
+    await hideKeyboard();
+    const loginSelector = '~Login button';
+    await $(loginSelector).click();
     //
     // 4. Validate that you are back on the products screen
+    // This is debatable, but I think that waiting for an element to be displayed
+    // is equal to asserting that it is displayed
+    await $('~products screen').waitForDisplayed({ timeout: 30000 });
   });
 
   it('Should be able to find the Sauce Labs Onesie and add it to the cart', async () => {
@@ -58,3 +85,24 @@ describe('My First Native App Test', () => {
     // 5. Validate that the cart has 1 item
   });
 });
+
+const hideKeyboard = async (): Promise<void> => {
+  // The hideKeyboard is not working on ios devices, so take a different approach
+  if (!(await driver.isKeyboardShown())) {
+    return;
+  }
+
+  if (driver.isIOS) {
+    await $('id=Return').click();
+  } else {
+    try {
+      await driver.hideKeyboard('pressKey', 'Done');
+    } catch (e) {
+      // Fallback
+      await driver.back();
+    }
+  }
+
+  // Wait for the keyboard animation to be done
+  await driver.pause(750);
+};
